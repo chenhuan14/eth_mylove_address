e649943089be59fcf8c5d513367e686e5c823160
chenhuan245 <245556819@qq.com>
2018-06-03 13:53:38 +0800

    use for window

--- mylove.py
+++ mylove.py
@@ -2,14 +2,15 @@
 # -*- coding: UTF-8 -*-
  
 import threading
-import commands
+import subprocess
 import sys
+import os
  
 exitFlag = 0
 
-mylove_first = "666"
+mylove_first = ""
 
-mylove_last = ""
+mylove_last = "8"
 
  
 class myThread (threading.Thread):   
@@ -26,10 +27,16 @@ def find_add(threadName):
     global exitFlag
 
     while(1):               
-        if exitFlag :
+        if exitFlag:
             break
-        status, result = commands.getstatusoutput('geth account new --password passwd --keystore ' + threadName)
-        address = result[result.find("{")+1:-1]
+			
+        cmd ='geth.exe account new --password passwd --keystore ' + threadName
+        ret = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = ret.communicate()
+		
+        add = str(result[0],encoding="utf8")
+
+        address = add[add.find("{")+1:-2]
         first = address[0:len(mylove_first)]
 
         if len(mylove_last) > 0:
@@ -37,20 +44,30 @@ def find_add(threadName):
         else:
             last = ""
 
-        print address
+        print (address)
         count = count + 1
 
         if first == mylove_first and last == mylove_last:
             print ("find it in " + threadName)
             exitFlag = 1
             break
+			
         if count % 5 == 0:
-            commands.getstatusoutput('rm -rf ' + threadName)
-    
+            cwd = os.getcwd()
+            del_file(cwd+'\\'+threadName)
+
+
+def del_file(path):
+    for i in os.listdir(path):
+        path_file=os.path.join(path,i)
+        if os.path.isfile(path_file):
+            os.remove(path_file)
+        else:
+            del_file(path_file)		
 
 threads = []
 # 创建新线程
-for i in range(3):
+for i in range(10):
     thread = myThread(i, "Thread-"+str(i))
     threads.append(thread)
 
